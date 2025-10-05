40d9b9ffe37331a93a21c49be0858c13e2ae4740
DashWood <iiviiohammad.nzm@gmail.com>
2025-10-05 13:23:23 +0330

    remove yml and back to jvm11

--- FTPHandler/build.gradle.kts
+++ FTPHandler/build.gradle.kts
@@ -9,54 +9,42 @@ plugins {
 android {
     namespace = "com.dashwood.ftphandler"
     compileSdk = 36
+    defaultConfig { minSdk = 21 }
 
-    defaultConfig {
-        minSdk = 21
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = false
-
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget = JvmTarget.JVM_17
-        }
+
+    publishing {
+        singleVariant("release") { withSourcesJar() }
     }
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                groupId = "com.github.dashwood01"
-                artifactId = "FTP-Handler"
-                version = "0.3.0"
+kotlin {
+    jvmToolchain(17)
+    compilerOptions { jvmTarget.set(JvmTarget.JVM_11) }
+}
 
-                from(components["release"])
-            }
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.github.dashwood01"
+            artifactId = "FTP-Handler"
+            version = "0.3.0"
+            afterEvaluate { from(components["release"]) }
         }
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.commons.net)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
     testImplementation(libs.junit)
-
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.commons.net)
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
 }
